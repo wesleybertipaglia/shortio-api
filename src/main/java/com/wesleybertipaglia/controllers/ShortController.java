3437138b7474e280f2aea783e82b8d50056f8d31
wesleybertipaglia <wesleybertipaglia@gmail.com>
2025-05-04 21:01:30 -0300

    feat(auth): update signin email and add frontend URL configuration feat(services): check permissions in resource retrieval chore(cors): configure CORS settings in application properties refactor(tests): remove unused GreetingResource integration and unit tests

--- src/main/java/com/wesleybertipaglia/controllers/ShortController.java
+++ src/main/java/com/wesleybertipaglia/controllers/ShortController.java
@@ -2,8 +2,11 @@ package com.wesleybertipaglia.controllers;
 
 import java.net.URI;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import com.wesleybertipaglia.services.ResourceService;
 
+import io.quarkus.security.identity.SecurityIdentity;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
@@ -16,16 +19,27 @@ public class ShortController {
     @Inject
     ResourceService resourceService;
 
+    @Inject
+    SecurityIdentity identity;
+
+    @ConfigProperty(name = "app.frontend.url")
+    String frontendUrl;
+
     @GET
     @Path("/{slug}")
     public Response redirect(@PathParam("slug") String slug) {
-        var resource = resourceService.getBySlug(slug);
-        var url = resource.url();
+        if (identity.isAnonymous()) {
+            var redirectUrl = frontendUrl + "/signin?redirect=/s/" + slug;
+            return Response.seeOther(URI.create(redirectUrl)).build();
+        }
 
+        var resource = resourceService.getBySlug(slug);
+
+        var url = resource.url();
         if (!url.matches("^(http|https)://.*")) {
             url = "https://" + url;
         }
 
-        return Response.seeOther(URI.create(url)).build();
+        return Response.status(Response.Status.OK).entity(url).build();
     }
 }

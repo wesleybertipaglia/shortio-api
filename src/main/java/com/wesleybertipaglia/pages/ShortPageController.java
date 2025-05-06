53a21612978bf4edbe73abdc11cf51f9e6303409
wesleybertipaglia <wesleybertipaglia@gmail.com>
2025-05-05 23:27:04 -0300

    feat: implement sign-in functionality with HTML form and backend integration

--- src/main/java/com/wesleybertipaglia/pages/ShortPageController.java
+++ src/main/java/com/wesleybertipaglia/pages/ShortPageController.java
@@ -1,8 +1,6 @@
-package com.wesleybertipaglia.controllers;
+package com.wesleybertipaglia.pages;
 
 import java.net.URI;
-
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import com.wesleybertipaglia.services.ResourceService;
 
@@ -13,33 +11,30 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 @Path("/s")
-@Produces(MediaType.APPLICATION_JSON)
+@Produces(MediaType.TEXT_HTML)
 @Consumes(MediaType.APPLICATION_JSON)
-public class ShortController {
+public class ShortPageController {
     @Inject
     ResourceService resourceService;
 
     @Inject
     SecurityIdentity identity;
 
-    @ConfigProperty(name = "app.frontend.url")
-    String frontendUrl;
-
     @GET
     @Path("/{slug}")
     public Response redirect(@PathParam("slug") String slug) {
         if (identity.isAnonymous()) {
-            var redirectUrl = frontendUrl + "/signin?redirect=/s/" + slug;
-            return Response.seeOther(URI.create(redirectUrl)).build();
+            var url = "/p/auth/signin?redirect=" + slug;
+            return Response.seeOther(URI.create(url)).build();
         }
 
         var resource = resourceService.getBySlug(slug);
-
         var url = resource.url();
+
         if (!url.matches("^(http|https)://.*")) {
             url = "https://" + url;
         }
 
-        return Response.status(Response.Status.OK).entity(url).build();
+        return Response.seeOther(URI.create(url)).build();
     }
 }
